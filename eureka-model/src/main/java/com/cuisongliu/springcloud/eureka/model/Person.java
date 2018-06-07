14dbda6af23db9578614d8d99936cf501e824380
cuisongliu <cuisongliu@qq.com>
2018-06-07 23:17:17 +0800

    集成 lombok

--- eureka-model/src/main/java/com/cuisongliu/springcloud/eureka/model/Person.java
+++ eureka-model/src/main/java/com/cuisongliu/springcloud/eureka/model/Person.java
@@ -23,60 +23,25 @@ package com.cuisongliu.springcloud.eureka.model;
  * THE SOFTWARE.
  */
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * @author cuisongliu [cuisongliu@qq.com]
  * @since 2018-06-06 下午9:53
  */
+@Data
 public class Person implements Serializable {
     private String name;
     private Integer age;
     private String sex;
     private String msg;
 
-    public Person(String name, Integer age, String sex) {
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-    }
-
     public Person(String name, Integer age, String sex, String msg) {
         this.name = name;
         this.age = age;
         this.sex = sex;
         this.msg = msg;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
     }
 }
